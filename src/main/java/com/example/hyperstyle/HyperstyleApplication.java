04abe31854cf9efef65bfab0b698dc0cd29438b7
Kazima-ken <kawatoxdx@gmail.com>
2025-11-04 23:18:07 +0700

    base

---
+++ src/main/java/com/example/hyperstyle/HyperstyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyperstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperstyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperstyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hyperstyle/HyperstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyperstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperstyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
